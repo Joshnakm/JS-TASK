50566aad6b2760823ddebce425fe4f0c454bb012
Joshnakm <152014129+Joshnakm@users.noreply.github.com>
2023-12-04 18:35:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+
+
+function changecontent(){
+   let x=document.getElementById("Know-more");
+    if(x.style.display=="block")
+{
+    x.style.display="none";
+
+}
+else{
+    x.style.display="block";
+}
+}
+function validation(){
+
+    if(document.getElementById("name-input").value=="")
+{
+    document.getElementById("name-validation").style.display="block";
+
+}
+if(document.getElementById("email-input").value=="")
+{
+    document.getElementById("email-validation").style.display="block";
+
+}
+if(document.getElementById("message-input").value=="")
+{
+    document.getElementById("message-validation").style.display="block";
+
+}
+return false;
+
+
+}
